f9f22001d1b109597742955daf83609ad128a543
Varshil-Shah <varshilashah09@gmail.com>
2021-12-28 16:04:32 +0530

    :sparkles: General structure to display categories

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/categories.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,26 +16,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("DeliMeal"),
-      ),
-      body: const Center(child: Text("Navigation time!")),
+      home: const CatogriesScreen(),
     );
   }
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
+import 'package:meal_app/widgets/category_item.dart';
 
 class CatogriesScreen extends StatelessWidget {
   const CatogriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20.0,
-        mainAxisSpacing: 20.0,
-        maxCrossAxisExtent: 200.0,
+    return Scaffold(
+      appBar: AppBar(title: const Text("DeliMeal")),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (categoryData) => CategoryItem(
+                title: categoryData.title,
+                color: categoryData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20.0,
+          mainAxisSpacing: 20.0,
+          maxCrossAxisExtent: 200.0,
+        ),
       ),
     );
   }
