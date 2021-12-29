6e0338080312a95e91b6b4faec567aab10e3ab0e
Varshil-Shah <varshilashah09@gmail.com>
2021-12-29 11:00:20 +0530

    :sparkles: Display meals according to selected category

--- lib/screens/category_meals.dart
+++ lib/screens/category_meals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
 
 class CategoryMeals extends StatelessWidget {
   static const routeName = "/category-meals";
@@ -13,12 +14,19 @@ class CategoryMeals extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final String categoryTitle = routeArgs["title"] as String;
     final String categoryId = routeArgs["id"] as String;
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: const Center(
-        child: Text("The Recipes For The Category"),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
