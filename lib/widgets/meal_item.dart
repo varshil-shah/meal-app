6029f928aee975fa988b1142249bf5622e20abaf
Varshil-Shah <varshilashah09@gmail.com>
2021-12-29 13:46:08 +0530

    :lipstick: Build meal_item information section

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -19,11 +19,36 @@ class MealItem extends StatelessWidget {
 
   void selectedMeal() {}
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return "Simple";
+      case Complexity.Challenging:
+        return "Challenging";
+      case Complexity.Hard:
+        return "Hard";
+      default:
+        return "Unknown";
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return "Affordable";
+      case Affordability.Pricey:
+        return "Pricey";
+      case Affordability.Luxurious:
+        return "Luxurious";
+      default:
+        return "Unknown";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: selectedMeal,
-      splashColor: Theme.of(context).primaryColor,
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(
@@ -47,8 +72,65 @@ class MealItem extends StatelessWidget {
                     height: 250,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20.0,
+                  right: 10.0,
+                  left: 10.0,
+                  child: Container(
+                    width: 220,
+                    color: Colors.black54,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5.0,
+                      horizontal: 15.0,
+                    ),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        const Icon(Icons.schedule),
+                        const SizedBox(width: 6),
+                        Text("$duration mins")
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        const Icon(Icons.work),
+                        const SizedBox(width: 6),
+                        Text(complexityText)
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        const Icon(Icons.attach_money),
+                        const SizedBox(width: 6),
+                        Text(affordabilityText)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
